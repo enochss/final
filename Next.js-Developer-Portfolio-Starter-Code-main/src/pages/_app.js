25d2315658b479246f6960872f7c88f7565da148
enochss <enochsaravanamuttu@gmail.com>
2024-09-12 18:27:59 +0100

    Update _app.js

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/_app.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/_app.js
@@ -1,10 +1,5 @@
 import '@/styles/globals.css'
-import {Inter} from "next/font/google"
 
-const inter = Inter({
-  subsets:['latin'],
-  variable: '--font-inter',
-})
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
